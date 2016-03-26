bbdfdf16e62bde74b41a28b38220820c108c05ca
AmWeRockStar <kotchaphan.m@kaidee.com>
2016-03-26 13:43:31 +0700

    Change custom to User class

---
+++ app/src/main/java/com/example/vee/git/User.java
@@ -0,0 +1,25 @@
+package com.example.vee.git;
+
+/**
+ * Created by วัชรัตน์ on 26/3/2559.
+ */
+public class User {
+    private String id;
+    private String fname;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+}
--- app/src/main/java/com/example/vee/git/custom.java
+++
@@ -1,7 +0,0 @@
-package com.example.vee.git;
-
-/**
- * Created by วัชรัตน์ on 26/3/2559.
- */
-public class custom {
-}
