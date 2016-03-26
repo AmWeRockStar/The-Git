def803be7d72aff4b9cce925d543d923cd96802c
Kimhuang <pronkamon10@gmail.com>
2016-03-26 15:03:31 +0700

    no message

--- app/src/main/java/com/example/vee/git/MainActivity.java
+++ app/src/main/java/com/example/vee/git/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.vee.git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Scene;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Scanner;
+
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText edtNumber1, edtNumber2;
+        Button btnPlus;
+        int result;
+
+        edtNumber1 = (EditText)findViewById(R.id.edtNumber1);
+        edtNumber2 = (EditText)findViewById(R.id.edtNumber2);
+        btnPlus = (Button) findViewById(R.id.btnPlus);
+
+        
     }
 }
