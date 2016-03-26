1d94b807dc648a5d1ae8bd167946d0ed3b3e6753
Kimhuang <pronkamon10@gmail.com>
2016-03-26 14:04:55 +0700

    no message

--- app/src/main/java/com/example/vee/git/custom.java
+++ app/src/main/java/com/example/vee/git/custom.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 
 
 public class custom extends BaseAdapter{
@@ -15,16 +16,18 @@ public class custom extends BaseAdapter{
     public custom(Context ctx, int id){
         this.context = ctx;
         this.id = id;
+
     }
 
     @Override
     public int getCount() {
         return 0;
+
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+      return 0;
     }
 
     @Override
